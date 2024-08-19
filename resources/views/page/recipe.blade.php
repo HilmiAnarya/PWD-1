06e8bc0fc0e92fa73beff9c6a19d9013e5c10f6c
Daffa Arief <hilmidrone@gmail.com>
2024-08-19 21:31:19 +0700

    recipe update

---
+++ resources/views/page/recipe.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../css/recipe.css">
+</head>
+<body>
+    <nav>
+        <div class="navbar">
+            <div class="left-row">
+                <p>Recipes</p>
+            </div>
+            <div class="logo">
+                <img src="../page/cucinare 2.png" alt="">
+            </div>
+            <div class="right-row">
+                <p>Profile</p>
+            </div>
+        </div>
+    </nav>
+
+    <div class="list-recipe">
+        <div class="col1">
+            test
+        </div>
+        <div class="col2">
+            huyah
+        </div>
+        <div class="col3">
+            pe
+        </div>
+    </div>
+    
+</body>
+</html>
