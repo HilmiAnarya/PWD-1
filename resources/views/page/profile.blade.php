be0cf80d6b8da63567b90dd45f46a308d82938b3
HilmiAnarya <hilmidrone@gmail.com>
2024-08-19 21:29:40 +0700

    recipe update

---
+++ resources/views/page/profile.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../css/profile.css">
+</head>
+<body>
+    <nav>
+        <div class="navbar">
+            <div class="left-row">
+                <p>LOGIN</p>
+            </div>
+            <div class="logo">
+                <img src="../page/cucinare 2.png" alt="">
+            </div>
+            <div class="right-row">
+                <p>Profile</p>
+                <p>--------------</p>
+                <p>Add Recipe</p>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container">
+        <div class="profile">
+            <h1>Profile</h1>
+            <h3>Here's your detail information about your account</h3>
+            <div class="detail">
+
+            </div>
+        </div>
+    </div>
+</body>
+</html>
