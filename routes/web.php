53766d19752bd213af74866d33c98de68fd90662
HilmiAnarya <hilmidrone@gmail.com>
2024-09-02 14:31:00 +0700

    route, controller update

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/recipe', function(){
+    return view('/page/recipe');
+});
+
+Route::get('/profile', function(){
+    return view('/page/profile');
+});
+
+Route::get('/login', function(){
+    return view('/login_regist/login');
+});
+
+Route::get('register', function(){
+    return view('/login_regist/register');
+});
