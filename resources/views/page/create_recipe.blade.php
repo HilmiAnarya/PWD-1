5110e7f072c06030c698741a9d055804159b1ce0
Daffa Arief <hilmidrone@gmail.com>
2024-11-01 00:45:54 +0700

    progress js pcr

--- resources/views/page/create_recipe.blade.php
+++ resources/views/page/create_recipe.blade.php
@@ -21,25 +21,35 @@
     <div class="create">
         <form action="{{route('showrecipe.store')}}" method="post" enctype="multipart/form-data">
             @csrf
-            <div class="namelab">
-                <label for class="form-label">Name:</label>
-            </div>
-            <input type="text" class="recipe" name="recipe_name" placeholder="Recipe name:">
-
-            <div class="desclab">
-                <label for class="form-label">Description:</label>
-            </div>
-            <textarea class="desc" name="desc" placeholder="Description:"></textarea>
-
             <div class="imagelab">
                 <label for class="form-label">image:</label>
             </div>
             <input type="file" class="img" name="img">
 
+            <div class="namelab">
+                <label for class="form-label">Name:</label>
+            </div>
+            <input type="text" class="recipe" name="recipe_name" placeholder="Recipe name:">
+
+            <div class="ingredientslab">
+                <label for class="form-label">Ingredients:</label>
+            </div>
+            <input type="text" name="ingredients[0]" class="ingredients" placeholder="Add ingredients:">
+            <button type="button" onclick="addIngredient()">Add Ingredient</button>
+            <button type="button" onclick="removeIngredient(this)">Remove</button>
+
+
+
+            <div class="steplab">
+                <label for class="form-label">Step:</label>
+            </div>
+            <input type="text" name="step_by_step[0]" class="step" placeholder="Add step:">
+            <button type="button" onclick="addStep()">Add Step</button>
+            <button type="button" onclick="removeStep(this)" >Remove</button>
+
             <div class="button">
                 <button type="submit" class="button">kirim</button>
             </div>
-
         </form>
     </div>
 
@@ -57,13 +67,7 @@
 
             ingredientDiv.innerHTML = `
             <label>Ingredient Name:</label>
-            <input type="text" name="ingredients[${ingredientIndex}][name]" required>
-
-            <label>Quantity:</label>
-            <input type="text" name="ingredients[${ingredientIndex}][quantity]" required>
-
-            <label>Unit:</label>
-            <input type="text" name="ingredients[${ingredientIndex}][unit]" required>
+            <input type="text" name="ingredients[${ingredientIndex}]" required>
 
             <button type="button" onclick="removeIngredient(this)">Remove</button>
         `;
